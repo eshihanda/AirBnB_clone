4ba783387368c2fcf11fa2d81049cb20c1ee951d
root <root@DESKTOP-J2I8KKI.localdomain>
2024-02-07 14:06:28 +0100

    Task 4 implemented

--- models/base_model.py
+++ models/base_model.py
@@ -2,29 +2,75 @@
 """defines a class base model with all common attributes
 and methods for other classes """
 
-import uuid
 from datetime import datetime
+import uuid
+
 
 class BaseModel:
-    """the base model class from which other classes inherit from"""
-    def __init__(self):
-        """initializes the base model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Base model class from which other classes inherit.
+
+    Attributes:
+        id (str): The unique identifier of the model instance.
+        created_at (datetime): The datetime when the instance was created.
+        updated_at (datetime): The datetime when the instance was last updated.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the base model.
+
+        Args:
+            *args: Variable positional arguments (Not used).
+            **kwargs: Variable keyword arguments.
+        """
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """returns string representation of the of the class"""
+        """Returns string representation of the class instance."""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the updated_attribute with current time"""
+        """Updates the updated_at attribute with the current time."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary with its keys and values"""
+        """Returns a dictionary representation of the class instance."""
         new_dict = self.__dict__.copy()
-        new_dict['__class__'] = type (self).__name__
+        new_dict['__class__'] = type(self).__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
