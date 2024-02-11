d1a6a1b01c43b1f09c0ea8f923557447d20d6a6d
eshihanda <eshihandamonn@gmail.com>
2024-02-11 23:21:31 +0300

    more classes

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a state class that inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """state class"""
+    name = ""
